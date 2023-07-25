a77d80669fb6d540d31cec8fd0da9d416676baee
orshah <orshpov@gmail.com>
2023-07-24 23:35:11 -0500

    added input

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 function Input() {
-  return <div>Input</div>;
+  return (
+    <div className="input">
+      <input type="text" placeholder="Type..." />
+      <div>
+        <img src="" alt="" />
+        <input type="file" />
+        <label htmlFor="file">
+          <img src="" alt="" />
+        </label>
+        <button>Send</button>
+      </div>
+    </div>
+  );
 }
 
 export default Input;
