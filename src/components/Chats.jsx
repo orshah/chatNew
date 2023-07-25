bd3c1fc84cce267d5cabb89bcf7d54108a327aa3
orshah <orshpov@gmail.com>
2023-07-24 22:33:10 -0500

    sidebar done

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,7 +1,40 @@
 import React from "react";
 
 function Chats() {
-  return <div>Chats</div>;
+  return (
+    <div>
+      <div className="userChat">
+        <img
+          src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=600&q=60"
+          alt="users avatars"
+        />
+        <div className="userChatInfo">
+          <span>James</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img
+          src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=600&q=60"
+          alt="users avatars"
+        />
+        <div className="userChatInfo">
+          <span>James</span>
+          <p>Bye</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img
+          src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=600&q=60"
+          alt="users avatars"
+        />
+        <div className="userChatInfo">
+          <span>James</span>
+          <p>GM</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Chats;
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 function Search() {
-  return <div>Search</div>;
+  return (
+    <div className="search">
+      <div className="searchForm">
+        <input placeholder="find a user" type="text" />
+      </div>
+      <div className="userChat">
+        <img
+          src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=600&q=60"
+          alt="users avatars"
+        />
+        <div className="userChatInfo">
+          <span>James</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Search;
