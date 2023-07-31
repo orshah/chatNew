aa6781854067608bf05a9353615b36ca737cd1fd
orshah <orshpov@gmail.com>
2023-07-31 14:55:05 -0500

    adding chat style

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,14 +1,16 @@
 import React from "react";
+import Img from "../img/img.png";
+import Attach from "../img/attach.png";
 
 function Input() {
   return (
     <div className="input">
       <input type="text" placeholder="Type..." />
-      <div>
-        <img src="" alt="" />
+      <div className="send">
+        <img src={Attach} alt="" />
         <input type="file" />
         <label htmlFor="file">
-          <img src="" alt="" />
+          <img src={Img} alt="" />
         </label>
         <button>Send</button>
       </div>
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 function Message() {
-  return <div>Message</div>;
+  return (
+    <div className="message">
+      <div className="messageInfo">
+        <img
+          src="https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1364&q=80"
+          alt=""
+        />
+        <span>just now</span>
+      </div>
+      <div className="messageContent">
+        <p>Hello</p>
+        <img
+          src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Message;
