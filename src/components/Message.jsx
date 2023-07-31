247316d81868ce29bb47b061946223da47602a7e
orshah <orshpov@gmail.com>
2023-07-31 15:10:02 -0500

    styling done

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Message() {
   return (
-    <div className="message">
+    <div className="message owner">
       <div className="messageInfo">
         <img
           src="https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1364&q=80"
