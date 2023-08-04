1d6f5111f6722d76a48535a320d96529066d0d9f
orshah <orshpov@gmail.com>
2023-08-03 19:57:31 -0500

    added nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,11 +9,8 @@ function Navbar() {
     <div className="navbar">
       <span className="logo">Chatzod</span>
       <div className="user">
-        <img
-          src="https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=600&q=60"
-          alt="my avatar"
-        />
-        <span>John</span>
+        <img src={currentUser.photoURL} alt="my avatar" />
+        <span>{currentUser.displayName}</span>
         <button onClick={() => signOut(auth)}>logout</button>
       </div>
     </div>
